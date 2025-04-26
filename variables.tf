853bc0ebae9ca6d24937fa30c1d83b6bde89c4c2
sailavnya <138848250+sailavnya@users.noreply.github.com>
2025-04-26 17:07:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,14 @@
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
 variable "my_bucket_region" {
     description = "my default bucket region"
     type = string
