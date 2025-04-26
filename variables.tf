4cfa88ae2a7c0adce039fecfe0cf226038889fb5
sailavnya <138848250+sailavnya@users.noreply.github.com>
2025-04-26 16:36:52 +0530

    Update variables.tf
    changed variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "my_bucket_region" {
     description = "my default bucket region"
     type = string
-    default = "eu-west-2"
+    default = "eu-north-1"
 }
 
 variable "my_bucket_name" {
     description = "my bucket name"
     type = string
-    default = "my-meme-card-game"
+    default = "terrform-github-actions-s3-2604"
 }
