c505252ab928e03abe0b8b57743fd02ec38d91d7
sailavnya <138848250+sailavnya@users.noreply.github.com>
2025-04-26 16:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 # AWS S3 bucket resource
 
+provider "aws" {
+  region = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
 resource "aws_s3_bucket" "test-bucket" {
   bucket = var.my_bucket_name # Name of the S3 bucket
 }
