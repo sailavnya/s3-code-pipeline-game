76880e6a5ecc3e1a342b35bb8bad60bb98fa8a96
sailavnya <138848250+sailavnya@users.noreply.github.com>
2025-04-26 18:32:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,5 @@ variable "my_bucket_region" {
 variable "my_bucket_name" {
     description = "my bucket name"
     type = string
-    default = "terrform-github-actions-s3-26041"
+    default = "terrform-github-actions-s3-2604"
 }
